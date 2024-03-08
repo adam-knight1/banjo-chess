5848630b643714e820c066bab862d8758014bbf0
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-08 17:15:03 -0500

    Added method doc for Knight.isValidMove

--- src/main/java/org/banjochess/piecelogic/Knight.java
+++ src/main/java/org/banjochess/piecelogic/Knight.java
@@ -8,26 +8,37 @@ public class Knight extends Piece {
         super(isWhite);
     }
 
+    /** This method checks to make sure the Knight's movement follows an L shape
+     * and that it doesn't land on a piece of the same color.
+     * @param board
+     * @param startX
+     * @param startY
+     * @param destinationX
+     * @param destinationY
+     * @return True if valid move, false if invalid (wrong shape, same color piece on destination square)
+     */
     @Override
     public boolean isValidMove(Board board, int startX, int startY, int destinationX, int destinationY) {
         //calculating the differences between start and end X and Y points
-        int deltaX = Math.abs(destinationX-startX);
-        int deltaY = Math.abs(destinationY-startY);
+        int deltaX = Math.abs(destinationX - startX);
+        int deltaY = Math.abs(destinationY - startY);
 
         //going to check for L shaped movement
-            //two square down X and one up Y or
+        //two square down X and one up Y or
         //  two square up Y and acros X
 
-        boolean isValidMove = ((deltaX == 2 && deltaY == 1)) || (deltaX ==1 && deltaY == 2); //Determine if piece moved in L shape
+        boolean isValidMove = ((deltaX == 2 && deltaY == 1)) || (deltaX == 1 && deltaY == 2); //Determine if piece moved in L shape
 
         if (!isValidMove) {
             return false;
         }
 
-        Piece destinationPiece = board.getPiece(destinationX,destinationY); //find out what piece is on the landing spot for the knight
+        Piece destinationPiece = board.getPiece(destinationX, destinationY); //find out what piece is on the landing spot for the knight
 
-        if (destinationPiece != null && destinationPiece.isWhite() == this.isWhite){ //can't land on same color piece
-        return false;
-    }
+        if (destinationPiece != null && destinationPiece.isWhite() == this.isWhite) { //can't land on same color piece
+            return false;
+        }
         return true; //move is valid!
+    }
 }
+
