78afeef5c69cc6b6e602af64c089b1f05befbd83
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2024-03-08 15:33:43 -0500

    continuing implementation of Bishop.isValidMove

--- src/main/java/org/banjochess/piecelogic/Bishop.java
+++ src/main/java/org/banjochess/piecelogic/Bishop.java
@@ -14,6 +14,18 @@ public class Bishop extends Piece {
         if (Math.abs(destinationX - startX) == (Math.abs(destinationY - startY))) {
              int xDirection = destinationX > startX ? 1: -1; //determine if piece is moving to the right
             //ie if the destination square as a higher value
+
+            int yDirection = destinationY > startY ? 1: -1; //positive is moving up along Y axis, negative moving down
+
+            int steps = Math.abs(destinationX - startX); //number of total squares moved (based on X axis movement)
+
+            //check for blocking pieces:
+
+            for (int i = 1; i < steps;i++){
+                //starting from 1 because of starting position and -1 because of destination capture
+                int xCurrent = startX + i * xDirection;
+
+
         }
         return false;
     }
